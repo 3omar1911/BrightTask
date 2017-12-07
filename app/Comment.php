df81ea5585b904227e3a9d7102e31414b7a37241
3omar1911 <oa36345@gmail.com>
2017-12-07 11:54:27 +0200

    defining the relationships inside the model classes

--- app/Comment.php
+++ app/Comment.php
@@ -4,7 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Comment extends Model
-{
-    //
+class Comment extends Model {
+    
+	protected $fillable = [
+		'content', 'user_id', 'post_id'
+	];
+
+    public function user() {
+
+    	return $this->belongsTo('App\User');
+    }
 }
--- app/Post.php
+++ app/Post.php
@@ -4,7 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
-{
-    //
+class Post extends Model {
+
+	protected $fillable = [
+		'title', 'content', 'user_id', 'category_id'
+	];
+
+    public function user() {
+
+    	return $this->belongsTo('App\User');
+    }
 }
