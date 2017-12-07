572f6d59e43387fc5b6141eab14c9228fb8d37ff
3omar1911 <oa36345@gmail.com>
2017-12-07 11:57:34 +0200

    defining the missing relationship between posts and categories

--- app/Category.php
+++ app/Category.php
@@ -4,7 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
-{
-    //
+class Category extends Model {
+    
+    protected $fillable = [
+    	'name'
+    ];
+
+    public function posts() {
+
+        return $this->hasMany('App\Post');
+    }
 }
--- app/Post.php
+++ app/Post.php
@@ -14,4 +14,9 @@ class Post extends Model {
 
     	return $this->belongsTo('App\User');
     }
+
+    public function category() {
+
+    	return $this->belongsTo('App\Category');
+    }
 }
